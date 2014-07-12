e1053bdaac9897bb42f33fd54ce22bb319967089
Ryan Kleck <ryan@kleck.us>
2014-07-11 22:51:35 -0700

    iteration2

--- DistributedFileSystem/src/com/kleck/DistributedFileSystem/DFSClient.java
+++ DistributedFileSystem/src/com/kleck/DistributedFileSystem/DFSClient.java
@@ -1,8 +1,13 @@
 package com.kleck.DistributedFileSystem;
 
 import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -10,83 +15,119 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
-
 public class DFSClient {
-    public static void main(String[] args) throws IOException {
-        
-        //read hostname file, load into props
-        Properties props = loadParams();
-        String[] hosts = props.getProperty("servers").split(";");
-        String command = "";
-        
-        //get BufferedReader to read input from user
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        while((command = br.readLine()) != null) {
-        	//break out of here if the user types exit
-        	if(command.trim().equals("exit"))	
-        		break;
-        	
-        	//otherwise start client threads
-        	//and pass the command
-        	startClientThreads(hosts, command, false);  
-        }     
-    }
-
-    
-    //starts 1 client thread per host 
-    //passes the command to each client thread
-    //this method used in Testing
-	public static void startClientThreads(String[] hosts, String command, boolean isLogTest) {
-		//put ClientThreads in an arrayList for easier management
-		ArrayList<DFSClientThread> lct = new ArrayList<DFSClientThread>();
-		String tempCommand = command;
-		//start each client thread and wait for
-		//them to finish processing the command
-		if(!command.trim().equals("")) {
-		    for(int i=0;i<hosts.length;i++) {
-		    	String[] host = hosts[i].split(",");
-		    	if(!host[0].equals("") && !host[1].equals("")) {
-		            lct.add(new DFSClientThread(host[0], Integer.parseInt(host[1]), command));
-		            lct.get(i).start();	
-		    	}
-		    }	        
-		    
-		    //wait for all threads to return
-		    for(int i=0;i<lct.size();i++){
-		    	try {
-					lct.get(i).join();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-		    }
-		}
-
+	private Properties prop;
 	
+	public DFSClient() { 
+		this.prop = loadParams();
+		runClient();
 	}
-    
+	
+	private void runClient() {
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		while(true) {
+			//get the user command (put, get, delete)
+			String command = "";
+			System.out.print("Enter a command:");
+			try {
+				command = inFromUser.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			if(command.equals("quit")) {
+				break;
+			}
+				
+			
+			//get user input
+			if(command.split(" ").length > 3 || command.split(" ").length < 2) {
+				System.out.println("Invalid command");
+			}
+			else if(command.split(" ")[0].equals("put")) {
+				if(command.split(" ").length < 3) {
+					System.out.println("Invalid command");
+				}
+				else {
+					boolean fileCheck = checkLocalFilename(command.split(" ")[1]);
+					if(!fileCheck)
+						System.out.println("Could not find local file " + command.split(" ")[1]);
+					else {
+						spinUpThreads(command);
+					}
+				}
+			}
+			else if(command.split(" ")[0].equals("get")) {
+				if(command.split(" ").length < 3) {
+					System.out.println("Invalid command");
+				}
+				else {
+					spinUpThreads(command);
+				}
+			}
+			else if(command.split(" ")[0].equals("delete")) {
+				spinUpThreads(command);
+			}
+		}	
+	}
+	
+	private void spinUpThreads (String command) {
+		List<DFSClientThread> sends = new ArrayList<DFSClientThread>();
+		String[] hosts = prop.getProperty("servers").split(";");
+		
+		//get a list of the servers and ports
+	    for(int i=0;i<hosts.length;i++) {
+	    	String[] host = hosts[i].split(",");
+	    	if(!host[0].equals("") && !host[1].equals("")) {
+	    		//"rmi://localhost/DFSServer"
+	    		String rmiServer = "rmi://" + host[0] + "/" + host[0].replace(".", "") + host[1];
+	            sends.add(new DFSClientThread(rmiServer, command));
+	            sends.get(i).start();	
+	    	}
+	    }	        
+	    
+	    //wait for all threads to return
+	    for(int i=0;i<sends.size();i++){
+	    	try {
+	    		sends.get(i).join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	    }
+	}
+	
+	//main
+	public static void main(String[] args) {
+		new DFSClient();
+	}
+	
+	//see if file is valid
+	private boolean checkLocalFilename(String filePath) {
+		File f = new File(filePath);
+		boolean result = f.exists() && !f.isDirectory();
+		return result;
+	}
+	
 	//open property file to get the hostName and portNumber
 	public static Properties loadParams() {
 	    Properties props = new Properties();
 	    InputStream is = null;
-	    
 	    //load file
 	    try {
-	        File f = new File("./settings.prop");
+	        File f = new File("settings.prop");
 	        is = new FileInputStream(f);
-	 
 	        // Try loading properties from the file (if found)
 	        props.load(is);
 	        is.close();
 	    }
 	    catch (Exception e) { 
-	    	System.out.println("Did not find hostname file. Ensure it is in the same folder as the jar.");
+	    	System.out.println("Did not find property file settings.prop.\nEnsure it is in the same folder as the jar.");
 	    }
-	    
 	    return props;
 	}
+
 }
--- DistributedFileSystem/src/com/kleck/DistributedFileSystem/DFSClientThread.java
+++ DistributedFileSystem/src/com/kleck/DistributedFileSystem/DFSClientThread.java
@@ -1,26 +1,70 @@
 package com.kleck.DistributedFileSystem;
 
+
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 public class DFSClientThread extends Thread {
+	private DFSServerInterface dfsServer;
+	private String rmiServer;
 	private String command;
-	private String host;
-	private int port;
 	
-	public DFSClientThread(String host, int port, String command) {
+	public DFSClientThread(String rmiServer, String command) { 
+		this.rmiServer = rmiServer;
 		this.command = command;
-		this.host = host;
-		this.port = port;
 	}
 	
-	public void run() {
-		//this.dfsServer = (DFSServerInterface) Naming.lookup(rmiMaster);
+	public void run() {		
+		//sets security so you can connect to rmi
+		//"rmi://localhost/DFSServer"
+		System.setProperty("java.security.policy","security.policy");
+		
+		//set the DFS master initially
+		try {
+			this.dfsServer = (DFSServerInterface) Naming.lookup(rmiServer);
+			//get the user command (put, get, delete)		
+			if(command.split(" ")[0].equals("put")) {
+				Path pathToFile = Paths.get(command.split(" ")[1]);
+				this.dfsServer.put(command.split(" ")[2], Files.readAllBytes(pathToFile), true);
+			}
+			if(command.split(" ")[0].equals("get")) {
+				byte[] fileToSave = this.dfsServer.get(command.split(" ")[1], true);
+				FileOutputStream fos;
+				fos = new FileOutputStream("FromDFS/" + command.split(" ")[2]);
+				fos.write(fileToSave);
+				fos.close();
+			}
+			else if(command.split(" ")[0].equals("delete")) {
+				this.dfsServer.delete(command.split(" ")[1], true);
+			}		
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			//Thread.currentThread().interrupt();
+			//return;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	private boolean checkLocalFilename(String filePath) {
-		File f = new File(filePath);
-		boolean result = f.exists() && !f.isDirectory();
-		return result;
-	}	
-
 }
